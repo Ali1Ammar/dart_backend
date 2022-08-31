0300381435f7f7bdf97a1eb271098101813c8933
Ali <aliammarali125@gmail.com>
2022-08-31 19:12:05 +0300

    add lint

--- bin/main.dart
+++ bin/main.dart
@@ -1,5 +1,8 @@
 
-import '../lib/main.dart';
+// ignore_for_file: avoid_relative_lib_imports
+
 import 'package:shelf_plus/shelf_plus.dart';
+
+import '../lib/main.dart';
 
 void main() => shelfRun(init);
